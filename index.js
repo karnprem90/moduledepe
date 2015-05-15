f76f07fac971bf015512e9c275f8feb65bf2d24a
karnprem90 <karnprem@cronj.com>
2015-05-15 15:35:03 +0530

    Added file

---
+++ index.js
@@ -0,0 +1,16 @@
+// Load modules
+
+
+var classification = require('./routes');
+
+// API Server Endpoints
+exports.endpoint = [
+
+    { method: 'GET',  path: '/getClassificationAttributeHost', config: classification.getClassificationAttributeHost },
+    { method: 'POST', path: '/api/classificationSearch', config: classification.classificationPOST},
+    { method: 'POST', path: '/api/classificationGroupSearch', config: classification.classificationPOST},
+    { method: 'POST', path: '/api/attributeSearch', config: classification.classificationPOST},
+    { method: 'POST', path: '/api/attributeSectionSearch', config: classification.classificationPOST},
+    { method: 'POST', path: '/api/attributeList', config: classification.classificationPOST},
+    { method: 'GET',  path: '/api/attribute/{id}', config: classification.classificationGET}
+];
